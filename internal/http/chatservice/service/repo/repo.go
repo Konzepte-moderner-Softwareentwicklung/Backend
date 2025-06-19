06705475eda5f0b46e79530b50311775fd3c0c86
nilspolek <nils@polek.de>
2025-06-19 23:34:20 +0200

    feat: now there a explicit chats that can be created insted of messages that have a uuid thats 'the chat id' so now multiple

--- internal/http/chatservice/service/repo/repo.go
+++ internal/http/chatservice/service/repo/repo.go
@@ -14,7 +14,15 @@ type Message struct {
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
 
+type Chat struct {
+	ID        uuid.UUID   `json:"id" bson:"_id"`
+	UserIds   []uuid.UUID `json:"user_ids" bson:"user_ids"`
+	CreatedAt time.Time   `json:"created_at" bson:"created_at"`
+}
+
 type Repository interface {
-	GetHistory(id uuid.UUID) ([]Message, error)
-	SendMessage(message Message) error
+	GetHistory(chatId uuid.UUID) ([]Message, error)
+	SendMessage(message Message, chatId uuid.UUID) error
+	CreateChat(user ...uuid.UUID) (uuid.UUID, error)
+	GetChats(userId uuid.UUID) ([]Chat, error)
 }
